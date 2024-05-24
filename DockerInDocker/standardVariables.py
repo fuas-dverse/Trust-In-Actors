869ebdc4a5b08b6b4b44899bff1aed5f82d7efaf
Arenco Meevissen <479450@student.fontys.nl>
2024-05-24 07:43:53 +0200

    set to Kafka cloud

--- DockerInDocker/standardVariables.py
+++ DockerInDocker/standardVariables.py
@@ -1,11 +1,23 @@
+import os
 import docker
 from confluent_kafka import Producer,Consumer
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Initialize Docker client
 client = docker.from_env()
 
+config = {
+    'bootstrap.servers': os.environ.get("KAFKA_BOOTSTRAP_SERVER"),
+    'security.protocol': os.environ.get("KAFKA_SECURITY_PROTOCOL"),
+    'sasl.mechanisms': os.environ.get("KAFKA_SASL_MECHANISMS"),
+    'sasl.username': os.environ.get("KAFKA_SASL_USERNAME"),
+    'sasl.password': os.environ.get("KAFKA_SASL_PASSWORD"),
+}
+
 # Initialize Kafka producer
-producer = Producer({'bootstrap.servers':'localhost:9092'})
+producer = Producer(config)
 
 def delivery_callback(err, msg):
         if err:
@@ -14,9 +26,9 @@ def delivery_callback(err, msg):
             print("Produced event to topic {topic}: value = {value:12}".format(
                 topic=msg.topic(), value=msg.value().decode('utf-8')))
 
-consumer= Consumer({'bootstrap.servers': 'localhost:9092',
-                     'group.id':'my-group',
-                     })
+
+config['group.id'] = "group_id"
+consumer= Consumer(config)
 
 def send_response(json_message):
     producer.produce('DiD_response', json_message,callback=delivery_callback)
